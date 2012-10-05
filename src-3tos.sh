3e8133946e1b0b3f65f4b56bc87c2d62d8cfd8e9
Dan Siemon <dan@coverfire.com>
2012-10-04 21:15:37 -0400

    Add FQ_CODEL and lots of other cleanups.

--- src-3tos.sh
+++ src-3tos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ##
 # Dan Siemon <dan@coverfire.com>
+# http://www.coverfire.com
 #
 # This script attempts to create per host fairness on the network
 # and for each host three priority classes. The hierarchy looks like:
@@ -15,7 +16,8 @@
 #			|
 #	Flow Bucket 1  .. NUM_FLOW_BUCKETS [Flow QDiscs are named (HOST_BUCKET * 100 + 1 + 1):0]
 #
-# The 0->NUM_FLOW_BUCKETS exist under every high, normal and low class.
+# The 0->NUM_FLOW_BUCKETS exist under every high, normal and low class. SFQ, SFB and FQ_CODEL
+# have embedded classes per flow and do not use NUM_FLOW_BUCKETS.
 #
 # Yes, the class and QDisc naming is confusing and there are probably bugs
 # if you set the NUM_HOST_BUCKETS or NUM_FLOW_BUCKETS too high.
@@ -35,30 +37,31 @@ DEVICE="ppp0"
 NUM_HOST_BUCKETS=8
 
 # The number of flow buckets within each high, normal and low class.
-# Not sure what's the best way to determine this value.
-# If SFQ or SFB are used this value is not used.
+# If SFQ, SFB or FQ_CODEL are used this value is not used as these QDiscs
+# have many embedded queues.
 NUM_FLOW_BUCKETS=32
 
 ####
 # Bandwidth rates
 ####
 # All rates are kbit/sec.
-# RATE should be set to just under your upload link rate.
-RATE="780"
+# RATE should be set to just under your link rate.
+RATE="6450"
 # Below three rates should add up to RATE.
 # Priority of these classes is 1 (Higest) -> 3 (Lowest)
-RATE_1="180"
-RATE_2="500"
-RATE_3="100"
+RATE_1="1000"
+RATE_2="4450"
+RATE_3="1000"
 
 ####
 # Queue size
 ####
-# Size the queues for sane latency.
+# Size the queues for sane latency. Only used with the simple FIFO QDiscs
+# ie not SFQ, FQ_CODEL.
 # 1456 / (780000 / 8) = 14ms
 # Ex: 3 * 1456 = 4368 / (700000 / 8) = 49ms
 # Ex: 4 * 1456 = 5824 / (780000 / 8) = 59ms
-FIFO_LEN=4
+FIFO_LEN=10
 
 ####
 # How often to perturb the hashes.
@@ -66,66 +69,96 @@ FIFO_LEN=4
 # This should probably be on the order of minutes so as to avoid the packet
 # reordering which can happen when the flows are redistributed
 # into different queues.
-PERTURB=15
+#PERTURB=15
 PERTURB=300
 
 ####
-# packet overhead
+# Packet overhead
 ####
-# PPPoE overhead is 40 bytes (http://www.adsl-optimizer.dk/thesis/)
-# If you aren't using PPPoE you want to set this to 0.
-OVERHEAD=40
+# Examples:
+#   ADSL:
+#	- http://www.adsl-optimizer.dk/thesis/
+# 	(http://web.archive.org/web/20090422131547/http://www.adsl-optimizer.dk/thesis/)
+#	- If you are using ADSL you probably want LINKLAYER="atm" too.
+#   VDSL2 (without ATM) w/ PPPoE:
+#	- 40 bytes for 802.3
+#	- 8 bytes for PPPoE
+OVERHEAD=48
 
 ####
 # Set linklayer to one of ethernet,adsl (adsl == atm).
 ####
-LINKLAYER="adsl"
+#LINKLAYER="adsl"
+LINKLAYER="ethernet"
 
 ####
 # The MTU of the underlying interface.
 ####
-MTU="1456"
+MTU="1492"
 
 ####
 # The keys that are used to identify individual flows.
 ####
-# For 5-tuple (flow) fairness use
+# For 5-tuple (flow) fairness
 #FLOW_KEYS="src,dst,proto,proto-src,proto-dst"
-# For 5-tuple (flow) fairness with NAT use
+# For 5-tuple (flow) fairness when the same device is performing NAT
 FLOW_KEYS="nfct-src,nfct-dst,nfct-proto,nfct-proto-src,nfct-proto-dst"
-# For 5-tuple (flow) fairness with IPIP IPIPv6, IPv6IP, IPv6IPv6 tunnels (no NAT) use
-# This requires my recent patch to the flow classifier.
-#FLOW_KEYS="src,dst,proto,proto-src,proto-dst,tunnel-src,tunnel-dst,proto,tunnel-proto-src,tunnel-proto-dst"
 
 ####
 # The keys that are used to identify a host's traffic.
 ####
 # No NAT
 #HOST_KEYS="src"
-# With NAT
+# With local device doing NAT
 HOST_KEYS="nfct-src"
-# With IPIP IPIPv6, IPv6IP, IPv6IPv6 tunnels (no NAT)
-#HOST_KEYS="src,tunnel-src"
 
-# Set R2Q (HTB knob) low because of the low bitrates.
-# If your rates aren't low you might not need this. Remove it from the
-# HTB line below.
-R2Q=1
+# Set R2Q (HTB knob) low if you use low bitrates. You may see warning from the kernel
+# in /var/log/messages indicating this valuye should be modified.
+#R2Q=10
 
 ####
-# Type of queues
+# Choosing the type of queue for each class
 ####
 # For each of the traffic classes you can choose which QDisc to use by uncommenting
-# the appropriate function call.
+# the appropriate function call below.
 
 ###########################################
-# Nothing to change below here.
 ###########################################
+# Other than picking QDisc type there is nothing to change below here.
+###########################################
+###########################################
+
+######################
+# Expand the config variables to tc arguments if they are defined.
+######################
+if [ "${OVERHEAD}" != "" ]; then
+	OVERHEAD="overhead ${OVERHEAD}"
+fi
+
+if [ "${LINKLAYER}" != "" ]; then
+	LINKLAYER="linklayer ${LINKLAYER}"
+fi
+
+if [ "${R2Q}" != "" ]; then
+	R2Q="r2q ${R2Q}"
+fi
+
+if [ "${PERTURB}" != "" ]; then
+	PERTURB="perturb ${PERTURB}"
+fi
+
+######################
+# Util functions
+######################
 
 # TC QDisc and class IDs are in hex.
 function dec_to_hex {
 	echo `printf %x $1`
 }
+
+######################
+# Functions to create QDiscs at the leaves.
+######################
 
 function drr {
 	PARENT=$1
@@ -134,24 +167,35 @@ function drr {
 	# Create the QDisc.
 	tc qdisc add dev ${DEVICE} parent ${PARENT} handle ${HANDLE} drr
 
-	# Create NUM_FLOW_BUCKETS classes.
+	# Create NUM_FLOW_BUCKETS classes and add a pfifo_head_drop to each.
 	for J in `seq ${NUM_FLOW_BUCKETS}`; do
 		tc class add dev ${DEVICE} parent ${HANDLE} classid ${HANDLE}:`dec_to_hex ${J}` drr quantum ${MTU}
 		tc qdisc add dev ${DEVICE} parent ${HANDLE}:`dec_to_hex ${J}` pfifo_head_drop limit ${FIFO_LEN}
 	done
 
 	# Add a filter to direct the packets.
-	tc filter add dev ${DEVICE} prio 1 protocol ip parent ${HANDLE}: handle 1 flow hash keys ${FLOW_KEYS} divisor ${NUM_FLOW_BUCKETS} perturb ${PERTURB} baseclass ${HANDLE}:1
+	tc filter add dev ${DEVICE} prio 1 protocol ip parent ${HANDLE}: handle 1 flow hash keys ${FLOW_KEYS} divisor ${NUM_FLOW_BUCKETS} ${PERTURB} baseclass ${HANDLE}:1
 }
 
 function sfq {
 	PARENT=$1
 	HANDLE=$2
 
-	tc qdisc add dev ${DEVICE} parent ${PARENT} handle ${HANDLE} sfq limit ${FIFO_LEN} quantum ${MTU} divisor 1024
+	#tc qdisc add dev ${DEVICE} parent ${PARENT} handle ${HANDLE} sfq limit ${FIFO_LEN} quantum ${MTU} divisor 1024
+	tc qdisc add dev ${DEVICE} parent ${PARENT} handle ${HANDLE} sfq quantum ${MTU} divisor 1024
 
 	# Don't use the SFQ default classifier.
-	tc filter add dev ${DEVICE} prio 1 protocol ip parent ${HANDLE}: handle 1 flow hash keys ${FLOW_KEYS} divisor 1024 perturb ${PERTURB} baseclass ${HANDLE}:1
+	tc filter add dev ${DEVICE} prio 1 protocol ip parent ${HANDLE}: handle 1 flow hash keys ${FLOW_KEYS} divisor 1024 ${PERTURB} baseclass ${HANDLE}:1
+}
+
+function fq_codel {
+	PARENT=$1
+	HANDLE=$2
+
+	tc qdisc add dev ${DEVICE} parent ${PARENT} handle ${HANDLE} fq_codel quantum ${MTU} flows 4096
+
+	# Don't use the default classifier.
+	tc filter add dev ${DEVICE} prio 1 protocol ip parent ${HANDLE}: handle 1 flow hash keys ${FLOW_KEYS} divisor 4096 ${PERTURB} baseclass ${HANDLE}:1
 }
 
 function sfb {
@@ -162,7 +206,7 @@ function sfb {
 	tc qdisc add dev ${DEVICE} parent ${PARENT} handle ${HANDLE} sfb target 20 max 25 increment 0.005 decrement 0.0001
 
 	# TODO - Should this have divisor?
-	tc filter add dev ${DEVICE} prio 1 protocol ip parent ${HANDLE}: handle 1 flow hash keys ${FLOW_KEYS} divisor 1024 perturb ${PERTURB}
+	tc filter add dev ${DEVICE} prio 1 protocol ip parent ${HANDLE}: handle 1 flow hash keys ${FLOW_KEYS} divisor 1024 ${PERTURB}
 }
 
 function pfifo_head_drop {
@@ -179,74 +223,95 @@ function pfifo {
 	tc qdisc add dev ${DEVICE} parent ${PARENT} handle ${HANDLE} pfifo limit ${FIFO_LEN}
 }
 
-# Get the dividied rate values for use later.
+######################
+# The real work starts here.
+######################
+
+# Calculate the divided rate values for use later.
 DIV_RATE=`expr ${RATE} / ${NUM_HOST_BUCKETS}`
 DIV_RATE_1=`expr ${RATE_1} / ${NUM_HOST_BUCKETS}`
 DIV_RATE_2=`expr ${RATE_2} / ${NUM_HOST_BUCKETS}`
 DIV_RATE_3=`expr ${RATE_3} / ${NUM_HOST_BUCKETS}`
 
-echo "DIV_RATE:" ${DIV_RATE}
-echo "DIV_RATE_1:" ${DIV_RATE_1}
-echo "DIV_RATE_2:" ${DIV_RATE_2}
-echo "DIV_RATE_3:" ${DIV_RATE_3}
+echo "Rate per host (DIV_RATE):" ${DIV_RATE}
+echo "High priority rate per host (DIV_RATE_1):" ${DIV_RATE_1}
+echo "Normal priority rate per host (DIV_RATE_2):" ${DIV_RATE_2}
+echo "Low priority rate per host (DIV_RATE_3):" ${DIV_RATE_3}
 
-# Delete the existing qdiscs etc if they exist.
+# Delete any existing qdiscs if they exist.
 ${TC} qdisc del dev ${DEVICE} root
 
 # HTB QDisc at the root. Default all traffic into the prio qdisc.
-${TC} qdisc add dev ${DEVICE} root handle 1: htb r2q ${R2Q}
+${TC} qdisc add dev ${DEVICE} root handle 1: htb ${R2Q}
 
 # Create a top level class with the max rate.
-${TC} class add dev ${DEVICE} parent 1: classid 1:1 htb rate ${RATE}kbit prio 0 linklayer ${LINKLAYER} overhead ${OVERHEAD}
+${TC} class add dev ${DEVICE} parent 1: classid 1:1 htb rate ${RATE}kbit prio 0 ${LINKLAYER} ${OVERHEAD}
 
-###
+######
 # Create NUM_HOST_BUCKETS classes within the top-level class.
-###
+# Within each of these create three priority classes.
+######
 for HOST_NUM in `seq ${NUM_HOST_BUCKETS}`; do
 	echo "Create host class:" $HOST_NUM
 
 	QID=`expr ${HOST_NUM} '+' 9` # 1+9=10 - Start classes at 10.
-	tc class add dev ${DEVICE} parent 1:1 classid 1:${QID} htb rate ${DIV_RATE}kbit ceil ${RATE}kbit prio 0 linklayer ${LINKLAYER} overhead ${OVERHEAD}
+	tc class add dev ${DEVICE} parent 1:1 classid 1:${QID} htb rate ${DIV_RATE}kbit ceil ${RATE}kbit prio 0 ${LINKLAYER} ${OVERHEAD}
 
-	###
+	######
 	# Within each host bucket add three sub-classes, high, normal and low priority.
 	# Priority classes are named 1:[HOST_BUCKET * 100 + 1]
-	###
+	######
 	QID_1=`expr $QID '*' 100 + 1`
-	tc class add dev ${DEVICE} parent 1:${QID} classid 1:${QID_1} htb rate ${DIV_RATE_1}kbit ceil ${RATE}kbit prio 0 linklayer ${LINKLAYER} overhead ${OVERHEAD}
+	tc class add dev ${DEVICE} parent 1:${QID} classid 1:${QID_1} htb rate ${DIV_RATE_1}kbit ceil ${RATE}kbit prio 0 ${LINKLAYER} ${OVERHEAD}
 	QID_2=`expr $QID '*' 100 + 2`
-	tc class add dev ${DEVICE} parent 1:${QID} classid 1:${QID_2} htb rate ${DIV_RATE_2}kbit ceil ${RATE}kbit prio 1 linklayer ${LINKLAYER} overhead ${OVERHEAD}
+	tc class add dev ${DEVICE} parent 1:${QID} classid 1:${QID_2} htb rate ${DIV_RATE_2}kbit ceil ${RATE}kbit prio 1 ${LINKLAYER} ${OVERHEAD}
 	QID_3=`expr $QID '*' 100 + 3`
-	tc class add dev ${DEVICE} parent 1:${QID} classid 1:${QID_3} htb rate ${DIV_RATE_3}kbit ceil ${RATE}kbit prio 2 linklayer ${LINKLAYER} overhead ${OVERHEAD}
+	tc class add dev ${DEVICE} parent 1:${QID} classid 1:${QID_3} htb rate ${DIV_RATE_3}kbit ceil ${RATE}kbit prio 2 ${LINKLAYER} ${OVERHEAD}
+
+	#######
+	# Within each priority class uncomment one of the QDisc types.
+	#######
 
 	###
-	# Within each priority class add a QDisc.
+	# High priority class
 	###
 	QID_1_1=`expr ${QID_1} + 1`
+
 	# Choose one of the below QDiscs for this class.
 	#drr 1:${QID_1} ${QID_1_1}
-	sfq 1:${QID_1} ${QID_1_1}
+	#sfq 1:${QID_1} ${QID_1_1}
+	fq_codel 1:${QID_1} ${QID_1_1}
 	#sfb 1:${QID_1} ${QID_1_1}
 	#pfifo_head_drop 1:${QID_1} ${QID_1_1}
 	#pfifo 1:${QID_1} ${QID_1_1}
 
+	###
+	# Normal priority class
+	###
 	QID_2_1=`expr ${QID_2} + 1`
+
 	# Choose one of the below QDiscs for this class.
 	#drr 1:${QID_2} ${QID_2_1}
-	sfq 1:${QID_2} ${QID_2_1}
+	#sfq 1:${QID_2} ${QID_2_1}
+	fq_codel 1:${QID_2} ${QID_2_1}
 	#sfb 1:${QID_2} ${QID_2_1}
 	#pfifo_head_drop 1:${QID_2} ${QID_2_1}
 	#pfifo 1:${QID_2} ${QID_2_1}
 
+	###
+	# Low priority class
+	###
 	QID_3_1=`expr ${QID_3} + 1`
+
 	# Choose one of the below QDiscs for this class.
 	#drr 1:${QID_3} ${QID_3_1}
-	sfq 1:${QID_3} ${QID_3_1}
+	#sfq 1:${QID_3} ${QID_3_1}
+	fq_codel 1:${QID_3} ${QID_3_1}
 	#sfb 1:${QID_3} ${QID_3_1}
 	#pfifo_head_drop 1:${QID_3} ${QID_3_1}
 	#pfifo 1:${QID_3} ${QID_3_1}
 
-	###
+	######
 	# Add filters to classify based on the TOS bits.
 	# Only mask against the three (used) TOS bits. The final two bits are used for ECN.
 	# TOS field is XXXDTRXX.
@@ -263,7 +328,7 @@ for HOST_NUM in `seq ${NUM_HOST_BUCKETS}`; do
 	# The thinking behind the below rules is to use D as an indication of delay sensitive
 	# and T as an indication of background (big transfer). All other combinations are put into
 	# default which is effectively a medium priority.
-	###
+	######
 
 	# D bit set.
 	tc filter add dev ${DEVICE} parent 1:${QID} protocol ip prio 10 u32 match ip tos 0x10 0x1c flowid 1:${QID_1}
@@ -284,4 +349,4 @@ done
 ${TC} filter add dev ${DEVICE} prio 1 protocol ip parent 1:0 u32 match u32 0 0 flowid 1:1
 
 # From the top level class hash into the host classes.
-${TC} filter add dev ${DEVICE} prio 1 protocol ip parent 1:1 handle 1 flow hash keys ${HOST_KEYS} divisor ${NUM_HOST_BUCKETS} perturb ${PERTURB} baseclass 1:10
+${TC} filter add dev ${DEVICE} prio 1 protocol ip parent 1:1 handle 1 flow hash keys ${HOST_KEYS} divisor ${NUM_HOST_BUCKETS} ${PERTURB} baseclass 1:10
